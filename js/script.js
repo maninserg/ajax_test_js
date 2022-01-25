86836ca481923d0de2974ab09bf9ffbe2d7108e4
maninserg <manin.serg@gmail.com>
2022-01-25 14:47:03 +0300

    Create func getPosts, json toarray, add listener to button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-// Create xhr object
-const xhr = new XMLHttpRequest();
+// find button on page
+const btn = document.querySelector('button');
 
-// Setting of request .open(method, url)
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+function getPosts(cb) {
+    // Create xhr object
+    const xhr = new XMLHttpRequest();
 
-// Add Listener of load data from server
-xhr.addEventListener('load', () => {
-    console.log(xhr.responseText);
+    // Setting of request .open(method, url)
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+    // Add Listener of load data from server
+    xhr.addEventListener('load', () => {
+        // parse json to array of objects
+        const response = JSON.parse(xhr.responseText);
+        // response to callback function
+        cb(response);
+
+    });
+
+    // Add Listener of error
+    xhr.addEventListener('error', () => {
+        console.log('error');
+    });
+
+    // Send request to server
+    xhr.send();
+}
+
+// Add Listner to button
+btn.addEventListener('click', (e) => {
+    getPosts((response) => {
+        console.log(response);
+    });
 });
-
-// Add Listener of error
-xhr.addEventListener('error', () => {
-    console.log('error');
-});
-
-// Send request to server
-xhr.send();
